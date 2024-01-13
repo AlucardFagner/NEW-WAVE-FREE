408892f3a48bf3088ecca1aa88278b8a018c545b
DESKTOP-B8MI20L\aluca <alucardfagner@yahoo.com>
2024-01-13 11:06:14 -0300

    NEW WAVE FREE

---
+++ resources/[weapons]/armas/fxmanifest.lua
@@ -0,0 +1,47 @@
+
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+
+files {
+	"stream/*",
+	"stream/*/**",
+	"data/colt/weaponcomponents.meta",
+	"data/colt/pedpersonality.meta",
+	"data/colt/weaponanimations.meta",
+	"data/colt/weaponarchetypes.meta",
+	"data/colt/weapons_coltxm177.meta",
+	"data/fnfal/weaponcomponents.meta",
+	"data/fnfal/pedpersonality.meta",
+	"data/fnfal/weaponanimations.meta",
+	"data/fnfal/weaponarchetypes.meta",
+	"data/fnfal/weapons_fnfal.meta",	
+	"data/parafal/weaponcomponents.meta",
+	"data/parafal/pedpersonality.meta",
+	"data/parafal/weaponanimations.meta",
+	"data/parafal/weaponarchetypes.meta",
+	"data/parafal/weapons_parafal.meta"
+}
+
+
+data_file "WEAPONCOMPONENTSINFO_FILE" "data/colt/weaponcomponents.meta"
+data_file "PED_PERSONALITY_FILE" "data/colt/pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "data/colt/weaponanimations.meta"
+data_file "WEAPON_METADATA_FILE" "data/colt/weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "data/colt/weapons_coltxm177.meta"
+data_file "WEAPONINFO_FILE_PATCH" "data/colt/weapons_coltxm177.meta"
+
+data_file "WEAPONCOMPONENTSINFO_FILE" "data/fnfal/weaponcomponents.meta"
+data_file "PED_PERSONALITY_FILE" "data/fnfal/pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "data/fnfal/weaponanimations.meta"
+data_file "WEAPON_METADATA_FILE" "data/fnfal/weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "data/fnfal/weapons_fnfal.meta"
+data_file "WEAPONINFO_FILE_PATCH" "data/fnfal/weapons_fnfal.meta"
+
+
+data_file "WEAPONCOMPONENTSINFO_FILE" "data/parafal/weaponcomponents.meta"
+data_file "PED_PERSONALITY_FILE" "data/parafal/pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "data/parafal/weaponanimations.meta"
+data_file "WEAPON_METADATA_FILE" "data/parafal/weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "data/parafal/weapons_parafal.meta"
+data_file "WEAPONINFO_FILE_PATCH" "data/parafal/weapons_parafal.meta"
