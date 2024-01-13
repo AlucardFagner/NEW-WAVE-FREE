d10bd420b48f6117327767fc66a78044cc783b84
DESKTOP-B8MI20L\aluca <alucardfagner@yahoo.com>
2024-01-13 11:27:00 -0300

    NEW WAVE FREE

---
+++ resources/[addons]/veiculos_vip/audio/suzukigsxr1k/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+game "gta5"
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+data_file "AUDIO_SYNTHDATA" "audioconfig/suzukigsxr1k_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/suzukigsxr1k_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/suzukigsxr1k_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_suzukigsxr1k"
---
+++ resources/[addons]/veiculos_vip/audio/ta019b58/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta019b58_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta019b58_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta019b58'
